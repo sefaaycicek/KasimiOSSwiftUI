7f91922f4cd4303a1aa9dced26cbcf1e6bd6580d
Sefa Aycicek <saycicek@ipucubilisim.com.tr>
2024-11-08 09:51:26 +0300

    Initial Commit

---
+++ KasimiOSSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KasimiOSSwiftUI
+//
+//  Created by Sefa Aycicek on 8.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KasimiOSSwiftUI/KasimiOSSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KasimiOSSwiftUIApp.swift
+//  KasimiOSSwiftUI
+//
+//  Created by Sefa Aycicek on 8.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct KasimiOSSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
